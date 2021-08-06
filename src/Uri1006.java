af26c3b1fb2229ea40a0def6cf97c1f50d3da121
Yuri <yuricoelho33@gmail.com>
2021-08-05 21:45:26 -0300

    Resolução do exercício 1006 do URI.

---
+++ src/Uri1006.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Uri1006 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        double A = leitor.nextDouble();
+        double B = leitor.nextDouble();
+        double C = leitor.nextDouble();
+
+        double media = (A * 2 + B * 3 + C * 5) / 10;
+
+        System.out.printf("MEDIA = %.1f%n", media);
+    }
+}

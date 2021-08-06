e6fea0eec83afe9ba3ddd45db645a9f482d6b819
Yuri <yuricoelho33@gmail.com>
2021-08-05 21:19:00 -0300

    Resolução do exercício 1004 do URI.

---
+++ src/Uri1003.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Uri1003 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int A = leitor.nextInt();
+        int B = leitor.nextInt();
+        int PROD;
+
+        PROD = A * B;
+
+        System.out.println("PROD = " + PROD);
+    }
+}
